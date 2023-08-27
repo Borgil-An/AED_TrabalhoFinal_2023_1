8109fc18f1485f2092a4c5b43d1ff71731f6722a
Gustavo Augusto Reginatto <60835063+Borgil-An@users.noreply.github.com>
2023-08-27 15:09:38 -0300

    Update Questão02.c

--- Questão02.c
+++ Questão02.c
@@ -55,11 +55,11 @@ float* matrix_multiply_normal(float* matrix1, float* matrix2, unsigned int size)
 
     for (unsigned int i = 0; i < size; i++) {
         for (unsigned int j = 0; j < size; j++) {
-            float sum = 0.0;
+            float result = 0.0;
             for (unsigned int k = 0; k < size; k++) {
-                sum += matrix1[i * size + k] * matrix2[k * size + j];
+                result += matrix1[i * size + k] * matrix2[k * size + j];
             }
-            result[i * size + j] = sum;
+            result[i * size + j] = result;
         }
     }
 
@@ -75,11 +75,11 @@ float* matrix_multiply(float* matrix1, float* matrix2, unsigned int size) {
 
     for (unsigned int i = 0; i < size; i++) {
         for (unsigned int j = 0; j < size; j++) {
-            float sum = 0.0;
+            float result = 0.0;
             for (unsigned int k = 0; k < size; k++) {
-                sum += matrix1[i * size + k] * matrix2[k * size + j];
+                result += matrix1[i * size + k] * matrix2[k * size + j];
             }
-            result[i * size + j] = sum;
+            result[i * size + j] = result;
         }
     }
 
